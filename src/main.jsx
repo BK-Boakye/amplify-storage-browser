d5c3266eb0a5b7764a8fb360946dab8988feb84f
BK-Boakye <krishpy3+baff@gmail.com>
2025-05-02 13:43:05 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { Amplify } from "aws-amplify";
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'
+import outputs from "../amplify_outputs.json";
+Amplify.configure(outputs)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Authenticator>
+    <App />
+    </Authenticator>
+  </StrictMode>,
+)
